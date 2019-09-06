4a6e4435fe6372c312a36657c66273cbeed97f9e
Marina Keunecke <marina_keunecke@hotmail.com>
2019-09-05 22:51:16 -0300

    add 1002 problem

---
+++ 1002.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+'''
+A fórmula para calcular a área de uma circunferência é: area = π . raio2.
+Considerando para este problema que π = 3.14159:
+
+- Efetue o cálculo da área, elevando o valor de raio ao quadrado e multiplicando por π.
+
+Entrada
+A entrada contém um valor de ponto flutuante (dupla precisão), no caso, a variável raio.
+
+Saída
+Apresentar a mensagem "A=" seguido pelo valor da variável area, conforme exemplo abaixo,
+com 4 casas após o ponto decimal. Utilize variáveis de dupla precisão (double). 
+'''
+
+raio = input()
+area = 3.14159 * float(raio) ** 2
+print("A=" + str(format(area, '.4f')))
