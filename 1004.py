1467e7452bbeac91eb26365770249f437e5bb134
Marina Keunecke <marina_keunecke@hotmail.com>
2019-09-08 20:47:27 -0300

    add problems 1003, 1004 and 1005

---
+++ 1004.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+'''
+Leia dois valores inteiros.
+A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD.
+A seguir mostre a variável PROD com mensagem correspondente.   
+
+Entrada
+O arquivo de entrada contém 2 valores inteiros.
+
+Saída
+Imprima a variável PROD conforme exemplo abaixo,
+com um espaço em branco antes e depois da igualdade. 
+'''
+
+PROD = int(input()) * int(input())
+print("PROD = " + str(PROD))
